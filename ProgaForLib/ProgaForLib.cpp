a2f9dc9e2c8914f0141a5c4ab9d4e2f5ea97fa2d
Ploff2033 <sergpilk33@gmail.com>
2024-06-03 23:26:57 +0300

    Добавьте файлы проекта.

---
+++ ProgaForLib/ProgaForLib.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <windows.h>
+#include <string>﻿
+#include <iostream>
+
+
+using namespace std;
+typedef bool (*search_function)(const char*, const char*);
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    HINSTANCE lib = LoadLibrary(TEXT("LibaCPlus.dll"));
+    search_function address = (search_function)GetProcAddress(lib, "search_function");
+
+    string input;
+    string find;
+
+    cout << "Введите слово: ";
+    cin >> input;
+
+    cout << "Введите буковки для поиска: ";
+    cin >> find;
+
+    if (lib != NULL) {
+        if (address != NULL) {
+            bool found = address(input.c_str(), find.c_str());
+
+            if (found) {
+                cout << "Все символы найдены.\n";
+            }
+            else {
+                cout << "Не все символы найдены.\n";
+            }
+        }
+        else {
+            cout << "Функция не найдена.\n";
+        }
+        FreeLibrary(lib);
+    }
+    else {
+        cout << "DLL не была загружена.\n";
+    }
+
+    return 0;
+}
